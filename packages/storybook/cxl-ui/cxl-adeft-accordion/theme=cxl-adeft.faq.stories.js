db5b8f0e006f75a05a1f7a13b33ead909cd79f76
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-08 11:45:10 +0100

    @vaadin/vaadin-checkbox installed

--- packages/storybook/cxl-ui/cxl-adeft-accordion/theme=cxl-adeft.faq.stories.js
+++ packages/storybook/cxl-ui/cxl-adeft-accordion/theme=cxl-adeft.faq.stories.js
@@ -1,9 +1,11 @@
 import { html } from 'lit-html';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import { registerGlobalStyles } from '@conversionxl/cxl-lumo-styles/src/utils';
 import cxlStoriesStyles from '@conversionxl/cxl-ui/src/styles/theme=cxl-adeft.faq.stories-css';
 import faqData from './theme=cxl-faq.data.json';
+import '@conversionxl/cxl-ui/src/components/cxl-adeft-accordion.js';
+import '@vaadin/vaadin-checkbox';
 
 export default {
   title: 'CXL UI|cxl-adeft-accordion',
@@ -11,21 +13,14 @@ export default {
 };
 
 const Template = () => {
-  const width = number('Comments Width', 200);
-  const height = number('Comments Height', 60);
-
   registerGlobalStyles(cxlStoriesStyles, {
-    moduleId: 'theme=cxl-adeft.faq.stories',
+    moduleId: 'theme=cxl-adeft-accordion.faq.stories',
   });
 
   return html`
     <style>
       .plural .entry-title {
         margin: 0;
-      }
-      textarea {
-        width: ${width}px;
-        height: ${height}px;
       }
       cxl-adeft-accordion {
         padding: 10px;
@@ -41,16 +36,16 @@ const Template = () => {
             class="${el.cxl_hybrid_attr_post['@attributes'].class}"
             theme="reverse"
           >
-            <div class="accordionSummary" slot="summary">
+            <div class="accordion-summary" slot="summary">
               <div class="left">
                 <vaadin-checkbox value="Option" theme="custom"></vaadin-checkbox>
               </div>
               <div class="right">
-                <div class="summaryTop">${unsafeHTML(el.title.rendered)}</div>
+                <div class="summary-top">${unsafeHTML(el.title.rendered)}</div>
               </div>
             </div>
             <vaadin-vertical-layout>
-              <div class="itemContent">
+              <div class="item-content">
                 <div class="checked">${unsafeHTML(el.content.rendered)}</div>
               </div>
             </vaadin-vertical-layout>
@@ -64,5 +59,5 @@ const Template = () => {
 export const CxlAdeftVaadinAccordionThemeFaq = Template.bind({});
 
 CxlAdeftVaadinAccordionThemeFaq.story = {
-  name: '[theme=cxl-adeft.faq.stories]',
+  name: '[theme=cxl-adeft-accordion.faq.stories]',
 };
