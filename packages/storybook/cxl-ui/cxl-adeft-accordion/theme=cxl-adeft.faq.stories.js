e13df1920f3055a1bf52a55adfc76e3e9ec41f5d
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-08 14:26:34 +0100

    cxl-adeft-accordion - clean component + story without additional commits

---
+++ packages/storybook/cxl-ui/cxl-adeft-accordion/theme=cxl-adeft.faq.stories.js
@@ -0,0 +1,57 @@
+import { html } from 'lit-html';
+import '@conversionxl/cxl-ui/src/components/cxl-app-layout.js';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import faqData from './theme=cxl-faq.data.json';
+
+export default {
+  title: 'CXL UI/cxl-adeft-accordion',
+};
+
+const Template = () => html`
+  <style>
+    .plural .entry-title {
+      margin: 0;
+    }
+    cxl-adeft-accordion {
+      padding: 10px;
+    }
+  </style>
+
+  <cxl-app-layout theme="2c-l" id="container">
+    <cxl-adeft-accordion
+      id="cxl-vaadin-accordion-26107"
+      theme="reverse"
+      class="cxl-adeft-accordion-class"
+    >
+      ${faqData.map(
+        (el) => html`
+          <vaadin-accordion-panel
+            id="${el.cxl_hybrid_attr_post['@attributes'].id}"
+            class="${el.cxl_hybrid_attr_post['@attributes'].class}"
+            theme="reverse"
+          >
+            <div slot="summary">
+              <div class="left">
+                <vaadin-checkbox value="Option" theme="custom"></vaadin-checkbox>
+              </div>
+              <div class="right">
+                <div class="summary-top">${unsafeHTML(el.title.rendered)}</div>
+              </div>
+            </div>
+            <vaadin-vertical-layout>
+              <div class="item-content">
+                <div>${unsafeHTML(el.content.rendered)}</div>
+              </div>
+            </vaadin-vertical-layout>
+          </vaadin-accordion-panel>
+        `
+      )}
+    </cxl-adeft-accordion>
+  </cxl-app-layout>
+`;
+
+export const CxlAdeftVaadinAccordionThemeFaq = Template.bind({});
+
+CxlAdeftVaadinAccordionThemeFaq.story = {
+  name: 'CXL UI/cxl-adeft-accordion',
+};
