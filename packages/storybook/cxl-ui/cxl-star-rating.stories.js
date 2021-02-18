a917caf2449fc6b665c24e6982a675d580dbc818
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-18 14:53:06 +0100

    chore(star-rating): added star rating + stories

---
+++ packages/storybook/cxl-ui/cxl-star-rating.stories.js
@@ -0,0 +1,31 @@
+import { html } from 'lit-html';
+import '@conversionxl/cxl-ui/src/components/cxl-app-layout.js';
+import '@conversionxl/cxl-ui/src/components/cxl-star-rating.js';
+
+/**
+ * 2021-01-14
+ * --> for future selves, machine elves
+ * all about knobs in storybook with examples
+ * https://github.com/storybookjs/storybook/tree/master/addons/knobs
+ */
+
+export default {
+  title: 'CXL UI/cxl-star-rating',
+};
+
+const valueChangedCallback = (event) => {
+  // eslint-disable-next-line no-console
+  console.log(event);
+};
+
+export const CXLStarRating = () => html`
+  <cxl-app-layout id="container" theme="2c-l">
+    <h2>CXL Star Rating</h2>
+    <cxl-star-rating
+      id="rating_post_id_1234"
+      @value-changed="${valueChangedCallback}"
+    ></cxl-star-rating>
+  </cxl-app-layout>
+`;
+
+CXLStarRating.storyName = 'cxl-star-rating';
