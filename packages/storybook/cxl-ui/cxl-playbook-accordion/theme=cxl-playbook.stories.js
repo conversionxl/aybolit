a27712428e6c3ed7480eb9d3d8a8b3acde45db8f
Dominik Stypuła <dominik.stypula@polcode.net>
2021-02-23 13:31:22 +0100

    chore(cxl-playbook.stories.js): removed cxl-app-layout

--- packages/storybook/cxl-ui/cxl-playbook-accordion/theme=cxl-playbook.stories.js
+++ packages/storybook/cxl-ui/cxl-playbook-accordion/theme=cxl-playbook.stories.js
@@ -15,38 +15,36 @@ export const CXLPlaybookAccordionTheme = () => html`
     }
   </style>
 
-  <cxl-app-layout theme="2c-l" id="container">
-    <cxl-playbook-accordion
-      id="cxl-vaadin-accordion-26107"
-      theme="reverse"
-      class="cxl-playbook-accordion-class"
-    >
-      ${faqData.map(
-        (el) => html`
-          <vaadin-accordion-panel
-            id="${el.cxl_hybrid_attr_post['@attributes'].id}"
-            class="${el.cxl_hybrid_attr_post['@attributes'].class}"
-            theme="reverse"
-          >
-            <vaadin-horizontal-layout theme="padding" slot="summary">
-              <div class="left">
-                <vaadin-checkbox value="Option" theme="custom"></vaadin-checkbox>
-              </div>
-              <div class="right">
-                <div class="summary-top">${unsafeHTML(el.title.rendered)}</div>
-              </div>
-            </vaadin-horizontal-layout>
+  <cxl-playbook-accordion
+    id="cxl-vaadin-accordion-26107"
+    theme="reverse"
+    class="cxl-playbook-accordion-class"
+  >
+    ${faqData.map(
+      (el) => html`
+        <vaadin-accordion-panel
+          id="${el.cxl_hybrid_attr_post['@attributes'].id}"
+          class="${el.cxl_hybrid_attr_post['@attributes'].class}"
+          theme="reverse"
+        >
+          <vaadin-horizontal-layout theme="padding" slot="summary">
+            <div class="left">
+              <vaadin-checkbox value="Option" theme="custom"></vaadin-checkbox>
+            </div>
+            <div class="right">
+              <div class="summary-top">${unsafeHTML(el.title.rendered)}</div>
+            </div>
+          </vaadin-horizontal-layout>
 
-            <vaadin-vertical-layout>
-              <div class="item-content">
-                <div>${unsafeHTML(el.content.rendered)}</div>
-              </div>
-            </vaadin-vertical-layout>
-          </vaadin-accordion-panel>
-        `
-      )}
-    </cxl-playbook-accordion>
-  </cxl-app-layout>
+          <vaadin-vertical-layout>
+            <div class="item-content">
+              <div>${unsafeHTML(el.content.rendered)}</div>
+            </div>
+          </vaadin-vertical-layout>
+        </vaadin-accordion-panel>
+      `
+    )}
+  </cxl-playbook-accordion>
 `;
 
 CXLPlaybookAccordionTheme.storyName = '[layout=2c-l] (default)';
