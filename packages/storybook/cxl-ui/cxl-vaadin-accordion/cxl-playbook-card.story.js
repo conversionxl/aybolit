c23d0d605f036444cc8d921d2a3da859c8547aa8
Dominik Stypuła <dominik.stypula@polcode.net>
2021-08-09 13:11:34 +0200

    feat: 2 versions visible always, with only 1 version we don't show the accordion

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-card.story.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-card.story.js
@@ -4,6 +4,8 @@ import '@conversionxl/cxl-ui/src/components/cxl-accordion-multiversion-card.js';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import jsonData from './playbook-card.data.json';
 
+const getRandomNumber = (min, max) => parseInt(Math.random() * (max - min) + min, 10);
+
 export const CXLPlaybookCard = () => html`
   <style>
     body {
@@ -103,24 +105,49 @@ export const CXLPlaybookCard = () => html`
             </h2>
 
             <div class="entry-byline">
-              <div>400 votes</div>
-              <div>Versions: ${html`<span class="author">${el.versions.length}</span>`}</div>
+              ${el.versions.length === 1
+                ? html`<div>400 votes</div>
+                    <div>Author: ${el.versions[0].author}</div>`
+                : html``}
+              ${el.versions.length > 1
+                ? html`<div>
+                    Versions: ${html`<span class="author">${el.versions.length}</span>`}
+                  </div>`
+                : html``}
             </div>
           </header>
 
           <div class="entry-content" itemprop="text">
             <div class="tags">${el.categories_names.map((t) => html`<a href="#">${t}</a>`)}</div>
 
-            <vaadin-details>
-              <div slot="summary">See more versions</div>
-              <div class="version-authors">
-                ${el.versions.map(
-                  (v) =>
-                    html`<div data-version-id="${v.version}">By ${v.author} (8888 upvotes)</div> `
-                )}
-              </div>
-            </vaadin-details>
+            ${el.versions.length > 1
+              ? html`
+                  <div class="version-authors">
+                    ${el.versions
+                      .slice(0, 2)
+                      .map(
+                        (v) =>
+                          html`<div data-version-id="${v.version}">
+                            By ${v.author} (${getRandomNumber(2, 1000)} upvotes)
+                          </div> `
+                      )}
+                  </div>
 
+                  <vaadin-details>
+                    <div slot="summary">See more versions</div>
+                    <div class="version-authors">
+                      ${el.versions
+                        .slice(2)
+                        .map(
+                          (v) =>
+                            html`<div data-version-id="${v.version}">
+                              By ${v.author} (${getRandomNumber(2, 1000)} upvotes)
+                            </div> `
+                        )}
+                    </div>
+                  </vaadin-details>
+                `
+              : html``}
             ${el.versions.map(
               (v) => html`
                 <div class="version" data-version-id="${v.version}">
