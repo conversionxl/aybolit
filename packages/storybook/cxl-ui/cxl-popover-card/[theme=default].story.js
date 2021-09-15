3fd6f123a434a9b630c635fba0abe944f5e0c20d
Dominik Stypuła <dominik.stypula@polcode.net>
2021-09-15 13:34:09 +0200

    feat: argsType introduced for strict story arguments, ability to use arrays, booleans etc

--- packages/storybook/cxl-ui/cxl-popover-card/[theme=default].story.js
+++ packages/storybook/cxl-ui/cxl-popover-card/[theme=default].story.js
@@ -1,12 +1,13 @@
 import { html } from 'lit-html';
 
-const Template = ({ apiUrl, text, href, userId, selected }) => html`
+const Template = ({ apiUrl, text, href, userId, postType, selected }) => html`
   <p>This is some paragraph</p>
   <p>This is some paragraph</p>
   <p>
     If you are providing a hyper-niche solution, a
     <cxl-popover-card
       apiUrl=${apiUrl}
+      postType=${postType}
       userId=${userId}
       ?selected=${selected}
       text=${text}
@@ -20,11 +21,37 @@ const Template = ({ apiUrl, text, href, userId, selected }) => html`
 export const CXLPopoverCardThemeDefault = Template.bind({});
 
 // more here: https://storybook.js.org/docs/web-components/essentials/controls#fully-custom-args
-CXLPopoverCardThemeDefault.args = {
-  apiUrl:
-    'https://cxl.fake/playbooks/1',
-  text: 'battle tested',
-  href: 'https://cxl.com',
-  userId: 555,
-  selected: false,
+// arg types: https://storybook.js.org/docs/web-components/essentials/controls#annotation
+CXLPopoverCardThemeDefault.argTypes = {
+  postType: {
+    options: ['playbook', 'minidegree'],
+    control: { type: 'select' },
+    name: 'Post type',
+    defaultValue: 'playbook',
+  },
+  apiUrl: {
+    name: 'API URL',
+    control: { type: 'text' },
+    defaultValue: 'https://cxl.fake/playbooks/1',
+  },
+  href: {
+    name: 'Anchor URL',
+    control: { type: 'text' },
+    defaultValue: 'https://cxl.com',
+  },
+  text: {
+    name: 'Anchor text',
+    control: { type: 'text' },
+    defaultValue: 'battle tested',
+  },
+  userId: {
+    name: 'User id',
+    control: { type: 'number' },
+    defaultValue: 123,
+  },
+  selected: {
+    name: 'Is selected',
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
 };
