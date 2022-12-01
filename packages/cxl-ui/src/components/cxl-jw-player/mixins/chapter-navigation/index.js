0abadbae47bcde0c05f6e60c24bd8ce873299ecf
saasfreelancer <syed.sayal.shah@gmail.com>
2022-12-01 18:29:35 +0500

    feat(cxl-ui): tweak chapter navigation mixin
    Co-authored-by: Andrew Noblet <andrewbnoblet@gmail.com>

--- packages/cxl-ui/src/components/cxl-jw-player/mixins/chapter-navigation/index.js
+++ packages/cxl-ui/src/components/cxl-jw-player/mixins/chapter-navigation/index.js
@@ -7,7 +7,7 @@ export function ChapterNavigationMixin(BaseClass) {
   class Mixin extends BaseClass {
     @property({ attribute: 'plugin-path', type: String }) pluginPath;
 
-    async __createChapterNavigation() {
+    async __setupChapterNavigation() {
       const chapters = await this.__getChapters();
 
       this.__chapterNavigation = document.createElement('div');
@@ -19,17 +19,11 @@ export function ChapterNavigationMixin(BaseClass) {
       this.__jwPlayerContainer.appendChild(this.__chapterNavigation);
 
       this.__jwPlayer.addButton(
-        `${this.pluginPath}images/chapter-bookmark-icon.svg`,
+        `<svg class="jw-player-button" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" aria-hidden="true" viewBox="0 0 1000 1000"><g><path d="M146 325c-42 0-67-26-67-63 0-37 25-63 67-63 42 0 67 26 67 63 0 37-25 63-67 63z m0 250c-42 0-67-26-67-63 0-37 25-63 67-63 42 0 67 26 67 63 0 37-25 63-67 63z m0 250c-42 0-67-26-67-63 0-37 25-63 67-63 42 0 67 26 67 63 0 37-25 63-67 63zM333 258c0-18 15-33 34-33h516c18 0 33 15 34 33 0 18-15 33-34 34H367c-18 0-33-15-34-34z m0 250c0-18 15-33 34-33h516c18 0 33 15 34 33s-15 33-34 34H367c-18 0-33-15-34-34z m0 250c0-18 15-33 34-33h516c18 0 33 15 34 33s-15 33-34 34H367c-18 0-33-15-34-34z"></path></g></svg>`,
         'Show Chapters',
         this.__toggleChapterNavigation.bind(this),
         'toggle-chapters'
       );
-    }
-
-    __addStyle() {
-      const el = document.createElement('style');
-      render(style, el);
-      this.appendChild(el);
     }
 
     __chapterSeek(e) {
@@ -40,8 +34,8 @@ export function ChapterNavigationMixin(BaseClass) {
     async __setup() {
       await super.__setup();
 
-      this.__addStyle();
-      this.__createChapterNavigation();
+      this.__addStyle(style);
+      this.__setupChapterNavigation();
     }
 
     __toggleChapterNavigation() {
