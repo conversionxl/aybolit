22de61a49263a11791cacc86fe1f40a830e693b4
Leho Kraav <leho@kraav.com>
2023-02-11 23:35:48 +0200

    feat(storybook): add @storybook/addon-storysource [1]
    1: https://storybook.js.org/addons/@storybook/addon-storysource/

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
   // @see https://github.com/storybookjs/storybook/issues/12307
   stories: ['../**/*.stories.js'],
-  addons: ['@storybook/addon-controls', '@storybook/addon-knobs', '@storybook/addon-viewport'],
+  addons: [
+    '@storybook/addon-controls',
+    '@storybook/addon-knobs',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
+  ],
 };
