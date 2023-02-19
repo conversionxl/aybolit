dcdbdc14a62d8157794c03a33ba033f7fc60fe63
Leho Kraav <leho@kraav.com>
2023-02-19 22:01:13 +0200

    chore: yarn lint:js --fix

--- packages/cxl-ui/src/components/cxl-checkout-details.js
+++ packages/cxl-ui/src/components/cxl-checkout-details.js
@@ -3,7 +3,7 @@ import { registerGlobalStyles } from '@conversionxl/cxl-lumo-styles/src/utils';
 import '@vaadin/details';
 import { Details } from '@vaadin/details/src/vaadin-details';
 import { customElement } from 'lit/decorators.js';
-import cxlCheckoutDetailsGlobalStyles from "../styles/global/cxl-checkout-details-css.js";
+import cxlCheckoutDetailsGlobalStyles from '../styles/global/cxl-checkout-details-css.js';
 
 @customElement('cxl-checkout-details')
 export class CXLCheckoutDetailsElement extends Details {
@@ -19,20 +19,21 @@ export class CXLCheckoutDetailsElement extends Details {
     });
 
     // Sanity check.
-    if (! this._checkoutFieldsSummaryElement) {
-      console.warn( 'Checkout fields summary element not found.');
+    if (!this._checkoutFieldsSummaryElement) {
+      /* eslint-disable-next-line no-console */
+      console.warn('Checkout fields summary element not found.');
       return;
     }
 
     // First render.
-    if (! this.opened) {
+    if (!this.opened) {
       this._onSummaryClosed();
     }
 
     // Panel toggles.
     this.addEventListener('opened-changed', () => {
       // Fire only when closing.
-      if (! this.opened) {
+      if (!this.opened) {
         this._onSummaryClosed();
       }
 
@@ -44,47 +45,48 @@ export class CXLCheckoutDetailsElement extends Details {
     const fieldsAttr = this.getAttribute('fields');
 
     // Sanity check.
-    if (! fieldsAttr) {
+    if (!fieldsAttr) {
       return '';
     }
 
     // Engage.
-    const fields = fieldsAttr.split(' ');
+    const fields = fieldsAttr.split(' ').map((field) => ({
+      name: field.replace('*', ''),
+      required: field.indexOf('*') !== -1,
+    }));
     const values = [];
 
     fields.forEach((field) => {
-      const isRequired = ( field.indexOf('*') !== -1 );
-
-      field = field.replace('*', '');
-
-      const el = this.querySelector(`#${field}`) || this.querySelector(`[name=${field}]`);
+      const el = this.querySelector(`#${field.name}`) || this.querySelector(`[name=${field.name}]`);
 
       if (el && el.value) {
         let value;
 
         // @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/labels
-        if ('input' === el.localName && 'radio' === el.type) {
+        if (el.localName === 'input' && el.type === 'radio') {
           // Skip to next field?
-          if (! el.checked) {
+          if (!el.checked) {
             return;
           }
 
           // Summarize label.
+          /* eslint-disable-next-line no-restricted-syntax */
           for (const label of el.labels) {
             value = label.textContent;
           }
         }
 
         // @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement#get_information_about_the_selected_option
-        if ('select' ===  el.localName) {
+        if (el.localName === 'select') {
           value = el.options[el.selectedIndex].label;
         }
 
         values.push(value || el.value);
       } else {
         // Avoid alerts for non-required fields.
-        if (isRequired) {
-          values.push('<vaadin-icon icon="lumo:cross"></vaadin-icon>')
+        /* eslint-disable-next-line no-lonely-if */
+        if (field.required) {
+          values.push('<vaadin-icon icon="lumo:cross"></vaadin-icon>');
         }
       }
     });
