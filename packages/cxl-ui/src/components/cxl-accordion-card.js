d09ce3ad46ef77c133ff06319a8487a364c4dad7
Andrew Noblet <andrewbnoblet@gmail.com>
2023-09-11 11:42:54 -0400

    refactor(cxl-ui): [cxl-accordion-card] remove click event listener
    This has been fixed upstream: https://github.com/vaadin/web-components/pull/6358/files

--- packages/cxl-ui/src/components/cxl-accordion-card.js
+++ packages/cxl-ui/src/components/cxl-accordion-card.js
@@ -4,17 +4,6 @@ import { AccordionPanel } from '@vaadin/accordion/src/vaadin-accordion-panel';
 
 @customElement('cxl-accordion-card')
 export class CXLAccordionCardElement extends AccordionPanel {
-  ready() {
-    super.ready();
-
-    // Stop card open on title click.
-    this.querySelectorAll(':any-link').forEach((el) => {
-      el.addEventListener('click', (e) => {
-        e.stopPropagation();
-      });
-    });
-  }
-
   static get is() {
     return 'cxl-accordion-card';
   }
