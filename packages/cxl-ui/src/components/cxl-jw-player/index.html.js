e179515b3ce9a16e705e0c0c7bab967775dcf93d
Andrew Noblet <andrewbnoblet@gmail.com>
2024-10-03 14:19:15 +0000

    fix(cxl-ui): cxl-jw-player setup crashes when transcripts aren't turned on
    https://app.clickup.com/t/apru1v?comment=90140063544642

--- packages/cxl-ui/src/components/cxl-jw-player/index.html.js
+++ packages/cxl-ui/src/components/cxl-jw-player/index.html.js
@@ -7,51 +7,47 @@ export const template = function () {
   return html`
     ${this.ready
       ? html`
-          <div class="grid height-100" id="container">
+          <div class="grid height-100" ?hidden=${this.captions} id="container">
             <slot></slot>
-            ${this.captions
-              ? html`
-                  <div class="center gap padding search">
-                    <vaadin-text-field
-                      @input=${this._debouncedSearch}
-                      clear-button-visible
-                      id="search"
-                      placeholder="Type and press enter to search"
-                      theme="small"
-                    >
-                      <vaadin-icon slot="prefix" icon="lumo:search"></vaadin-icon>
-                    </vaadin-text-field>
-                    ${this._isSearchMinimumLength
-                      ? html`<div id="search-result-count">(${this._matches} matches)</div>`
-                      : nothing}
-                    <vaadin-checkbox
-                      @change=${this._toggleShouldScroll}
-                      ?checked=${this._shouldScroll}
-                      label="Scroll"
-                    ></vaadin-checkbox>
-                  </div>
-                  <div class="captions padding scroll">
-                    ${this._tracks.map(
-                      (track, index) =>
-                        html`${track.data.text
-                          ? html`${track.isChapter
-                              ? html`<h2 @click=${this._onCaptionClick} data-index=${index}>
-                                  ${track.data.text}
-                                </h2>`
-                              : html`
-                                  <span
-                                    @click=${this._onCaptionClick}
-                                    ?active=${this._currentTrack === index}
-                                    data-index=${index}
-                                  >
-                                    ${track.data.text}
-                                  </span>
-                                `}`
-                          : nothing}`
-                    )}
-                  </div>
-                `
-              : nothing}
+            <div class="center gap padding search">
+              <vaadin-text-field
+                @input=${this._debouncedSearch}
+                clear-button-visible
+                id="search"
+                placeholder="Type and press enter to search"
+                theme="small"
+              >
+                <vaadin-icon slot="prefix" icon="lumo:search"></vaadin-icon>
+              </vaadin-text-field>
+              ${this._isSearchMinimumLength
+                ? html`<div id="search-result-count">(${this._matches} matches)</div>`
+                : nothing}
+              <vaadin-checkbox
+                @change=${this._toggleShouldScroll}
+                ?checked=${this._shouldScroll}
+                label="Scroll"
+              ></vaadin-checkbox>
+            </div>
+            <div class="captions padding scroll">
+              ${this._tracks.map(
+                (track, index) =>
+                  html`${track.data.text
+                    ? html`${track.isChapter
+                        ? html`<h2 @click=${this._onCaptionClick} data-index=${index}>
+                            ${track.data.text}
+                          </h2>`
+                        : html`
+                            <span
+                              @click=${this._onCaptionClick}
+                              ?active=${this._currentTrack === index}
+                              data-index=${index}
+                            >
+                              ${track.data.text}
+                            </span>
+                          `}`
+                    : nothing}`
+              )}
+            </div>
           </div>
         `
       : nothing}
